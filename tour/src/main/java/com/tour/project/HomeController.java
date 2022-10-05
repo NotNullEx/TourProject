782851d4022a954b66fba4fde986da6eae7fde5f
kjmin <kjmin@LAPTOP-5RHBKJVO>
2022-10-05 18:43:03 +0900

    tset2

--- tour/src/main/java/com/tour/project/HomeController.java
+++ tour/src/main/java/com/tour/project/HomeController.java
@@ -32,7 +32,7 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
-		System.out.println("test");
+		System.out.println("tsss");
 		return "/front/about";
 	}
 	@RequestMapping(value = {"/front/blog"})
