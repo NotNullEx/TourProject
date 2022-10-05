383043794921d1f951d25332f7b81f1948fb08d1
kjmin <kjmin@LAPTOP-5RHBKJVO>
2022-10-05 18:53:57 +0900

    commit test

--- tour/src/main/java/com/tour/project/HomeController.java
+++ tour/src/main/java/com/tour/project/HomeController.java
@@ -32,7 +32,7 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
-		System.out.println("tsss");
+		System.out.println("wlrjfskjfs");
 		return "/front/about";
 	}
 	@RequestMapping(value = {"/front/blog"})
