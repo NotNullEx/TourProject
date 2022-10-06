038abbcbd3d083dfe9e8f098e8fa5df30b030864
User <User@Studyroom-04>
2022-10-06 18:33:30 +0900

    git test

---
+++ tour/src/main/java/com/tour/project/TestController.java
@@ -0,0 +1,27 @@
+package com.tour.project;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.mybatis.logging.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+	@RequestMapping(value = {"/"})
+	public String home(Locale locale, Model model) {
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		System.out.println("test");
+		System.out.println("teeeerrrr12345557441");
+		return "/front/about";
+	}
+}
