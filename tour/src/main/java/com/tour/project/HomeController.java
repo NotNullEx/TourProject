4670ddfc8d57bbe371c177e65499b629338f9886
User <User@Studyroom-04>
2022-10-06 16:56:32 +0900

    git test2

--- tour/src/main/java/com/tour/project/HomeController.java
+++ tour/src/main/java/com/tour/project/HomeController.java
@@ -32,7 +32,7 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
-		System.out.println("wlrjfskjfs");
+		System.out.println("test");
 		return "/front/about";
 	}
 	@RequestMapping(value = {"/front/blog"})
