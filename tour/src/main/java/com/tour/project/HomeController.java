bcae33b1b5df9bddf0edcd90ae6f9313b9199a2d
User <User@Studyroom-04>
2022-10-06 17:01:41 +0900

    test2

--- tour/src/main/java/com/tour/project/HomeController.java
+++ tour/src/main/java/com/tour/project/HomeController.java
@@ -33,6 +33,7 @@ public class HomeController {
 		
 		model.addAttribute("serverTime", formattedDate );
 		System.out.println("test");
+		System.out.println("test123123");
 		return "/front/about";
 	}
 	@RequestMapping(value = {"/front/blog"})
