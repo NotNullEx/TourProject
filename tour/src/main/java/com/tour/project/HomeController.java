db61caa7ed812587a4a45602631e2c7120e639a9
C-05 <C-05@DESKTOP-QDFQ5TU>
2022-10-09 14:53:01 +0900

    test

--- tour/src/main/java/com/tour/project/HomeController.java
+++ tour/src/main/java/com/tour/project/HomeController.java
@@ -32,7 +32,7 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
-		System.out.println("test");
+		System.out.println("test5");
 		System.out.println("teeeerrrr12345557441");
 		return "/front/about";
 	}
