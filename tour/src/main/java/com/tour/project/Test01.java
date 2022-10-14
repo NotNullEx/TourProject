6ed15fea0a737c7e3640bd82860b9598b813f793
kjmin <kjmin@LAPTOP-5RHBKJVO>
2022-10-14 17:13:56 +0900

    관광API 출력 테스트

--- tour/src/main/java/com/tour/project/Test01.java
+++ tour/src/main/java/com/tour/project/Test01.java
@@ -1,7 +1,71 @@
 package com.tour.project;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
 public class Test01 {
-	public static void main(String[] args) {
-		System.out.println("asfsafas");
+	
+	@RequestMapping(value = {"/front/test"})
+	public ModelAndView testView(Locale locale, Model model) {
+		
+		ModelAndView models = new ModelAndView("/front/test");
+		try {
+			
+			String key = "4f645452506b6a6d38307a53726f48";
+			StringBuilder urlBuilder = new StringBuilder("http://openapi.seoul.go.kr:8088"); /*URL*/
+			urlBuilder.append("/" +  URLEncoder.encode(key,"UTF-8") ); /*인증키 (sample사용시에는 호출시 제한됩니다.)*/
+			urlBuilder.append("/" +  URLEncoder.encode("json","UTF-8") ); /*요청파일타입 (xml,xmlf,xls,json) */
+			urlBuilder.append("/" + URLEncoder.encode("TbVwEntertainment","UTF-8")); /*서비스명 (대소문자 구분 필수입니다.)*/
+			urlBuilder.append("/" + URLEncoder.encode("1","UTF-8")); /*요청시작위치 (sample인증키 사용시 5이내 숫자)*/
+			urlBuilder.append("/" + URLEncoder.encode("5","UTF-8")); /*요청종료위치(sample인증키 사용시 5이상 숫자 선택 안 됨)*/
+			urlBuilder.append("/" + URLEncoder.encode("ko","UTF-8"));
+			// 상위 5개는 필수적으로 순서바꾸지 않고 호출해야 합니다.
+			
+			SimpleDateFormat sdf2 = new SimpleDateFormat("yyyyMMdd");
+			Date now = new Date();
+			String nowTime2 = sdf2.format(now);
+			
+			// 서비스별 추가 요청 인자이며 자세한 내용은 각 서비스별 '요청인자'부분에 자세히 나와 있습니다.
+			//urlBuilder.append("/" + URLEncoder.encode(nowTime2,"UTF-8")); /* 서비스별 추가 요청인자들*/
+			
+			URL url = new URL(urlBuilder.toString());
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");
+			conn.setRequestProperty("Content-type", "application/json");
+			System.out.println("Response code: " + conn.getResponseCode()); /* 연결 자체에 대한 확인이 필요하므로 추가합니다.*/
+			BufferedReader rd;
+			// 서비스코드가 정상이면 200~300사이의 숫자가 나옵니다.
+			if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+					rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			} else {
+					rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+			}
+			StringBuilder sb = new StringBuilder();
+			String line;
+			while ((line = rd.readLine()) != null) {
+					sb.append(line);
+			}
+			models.addObject("sb",sb);
+			rd.close();
+			conn.disconnect();
+			System.out.println(sb.toString());
+		}catch (Exception e) {
+			e.getStackTrace();
+		}
+		
+		return models;
+		
 	}
 }
