4c488dadae09217b9fae20f443919b35726dd451
kjmin <kjmin@LAPTOP-5RHBKJVO>
2022-11-07 16:48:59 +0900

    민경진: 공통 패키지 추가

---
+++ tour/src/main/java/com/tour/project/common/Criteria.java
@@ -0,0 +1,53 @@
+package com.tour.project.common;
+
+public class Criteria {
+
+	 /* 현재 페이지 */
+    private int pageNum;
+    
+    /* 한 페이지 당 보여질 게시물 갯수 */
+    private int amount;
+    
+    /* 스킵 할 게시물 수( (pageNum-1) * amount ) */
+    private int skip;
+    
+    /* 기본 생성자 -> 기봅 세팅 : pageNum = 1, amount = 10 */
+    public Criteria() {
+        this(1,10);
+    }
+    
+    /* 생성자 => 원하는 pageNum, 원하는 amount */
+    public Criteria(int pageNum, int amount) {
+        this.pageNum = pageNum;
+        this.amount = amount;
+        this.skip = (pageNum-1)*amount;
+    }
+
+	public int getPageNum() {
+		return pageNum;
+	}
+
+	public void setPageNum(int pageNum) {
+		this.skip = (pageNum-1)*this.amount;
+		this.pageNum = pageNum;
+	}
+
+	public int getAmount() {
+		this.skip = (pageNum-1)*this.amount;
+		return amount;
+	}
+
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+
+	public int getSkip() {
+		return skip;
+	}
+
+	public void setSkip(int skip) {
+		this.skip = skip;
+	}
+    
+    
+}
